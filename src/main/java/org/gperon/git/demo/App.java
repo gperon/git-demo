b773254f4062f3cf4d8fb92a91061253e5d047d3
Giorgio Peron <giorgioperon@luxottica.com>
2009-10-10 18:22:43 +0200

    Importing the git-demo project

---
+++ src/main/java/org/gperon/git/demo/App.java
@@ -0,0 +1,13 @@
+package org.gperon.git.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
