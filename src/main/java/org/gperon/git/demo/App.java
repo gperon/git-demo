b65b9737b06ca1b317fbc842c50126448f675171
Giorgio Peron <giorgioperon@luxottica.com>
2009-12-23 10:57:21 +0100

    formatted

--- src/main/java/org/gperon/git/demo/App.java
+++ src/main/java/org/gperon/git/demo/App.java
@@ -4,10 +4,9 @@ package org.gperon.git.demo;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        /*formattato*/
+        System.out.println("Hello World!");
     }
 }
